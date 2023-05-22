eac984231f7b2aa5307c1a2fb523840847983ae2
Thierry Davelaar <t.davelaar@sportsentmedia.com>
2023-05-22 11:00:26 +0200

    Feat: ✨ Add demo to app

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+def create_x_array(x: int) -> list:
+    return [i for i in range(x)]
+
+
+@app.route("/run-model")
+def run_model():
+    return "Running model"
+
+
+
+@app.route('/')
+def main():
+    x = 10_000_000
+    x_array = create_x_array(x)
+    print(x_array)
+    
+    return '<h1>Hello Data Team</h1>'
+    
+    
+if __name__ == '__main__':
+    app.run(debug=True)
